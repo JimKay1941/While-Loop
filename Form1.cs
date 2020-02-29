dcb44b701c05d91fdb36c46841337705cf3e4c4f
Jam Kay <jam@jimkay.us>
2020-02-29 12:08:09 -0700

    The example is fully tested and ready for creating a video.

--- Form1.cs
+++ Form1.cs
@@ -16,5 +16,43 @@ namespace While_Loop
         {
             InitializeComponent();
         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int[] dataArray = new int[3] {6,7,8};
+
+            doThisBefore();
+
+            int Index = 0;
+
+            while (Index < 3)
+            {
+                doSomething(dataArray[Index]);
+
+                Index += 1;   // this instruction is the same as: Index = Index + 1; Remember, this is NOT a mathematical equation, it is an 'assignment statement.' 
+            }
+
+            doThisAfter();
+        }
+
+        private void doThisBefore()
+        {
+            return;
+        }
+
+        private void doSomething(int oneElement)
+        {
+            return;
+        }
+
+        private void doThisAfter()
+        {
+            return;
+        }
     }
 }
